ab93f1c1ef040b516e8279877aa59ee00d158561
Thirunavukarasu17 <sthirunavukarasu112@gmail.com>
2024-03-05 21:08:02 +0530

    first commit

--- routes/api/stocks.js
+++ routes/api/stocks.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const stocksCtrl = require('../../controllers/api/stocks');
 const ensureLoggedIn = require("../../config/ensureLoggedIn");
 
-// All paths start with '/api/stocks'
 
 router.post('/', ensureLoggedIn, stocksCtrl.create);
 router.get('/:id', ensureLoggedIn, stocksCtrl.show);
